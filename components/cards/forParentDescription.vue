0a4b565d87293911d71d051789006fa03ae173d5
rafiarrafif <rafiarrafif05@gmail.com>
2024-09-18 21:20:59 +0700

    add description for parent section

---
+++ components/cards/forParentDescription.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex gap-6">
+    <div
+      class="flex bg-secondary-500 h-12 w-12 justify-center items-center rounded-lg"
+    >
+      <Icon :name="props.icon" :size="iconSize" class="bg-secondary-50" />
+    </div>
+    <div class="flex gap-1 flex-col w-72">
+      <h1 class="font-semibold text-secondary-50 text-lg">{{ props.title }}</h1>
+      <span class="text-darkGray-200">{{ props.description }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  icon: {
+    type: String,
+    default: "hugeicons:album-not-found-01",
+  },
+  iconSize: {
+    type: String,
+    default: "1.6em",
+  },
+});
+</script>
--- components/pages/landingpage/forParentDescription.vue
+++ components/pages/landingpage/forParentDescription.vue
@@ -3,32 +3,41 @@
     class="h-full w-148 rounded-tr-3xl rounded-br-3xl relative overflow-hidden"
   >
     <div id="items" class="z-10 relative">
-      <div class="flex">
-        <div class="flex">
-          <div
-            class="flex bg-secondary-500 h-12 w-12 justify-center items-center rounded-lg"
-          >
-            <Icon
-              name="solar:chart-bold-duotone"
-              size="1.6em"
-              class="bg-secondary-50"
-            />
-          </div>
-          <div>
-            <h1>Detailed Progress Reports</h1>
-            <span
-              >Gain insights into your child's academic performance, including
-              completed modules, test scores, and time spent on learning.</span
-            >
-          </div>
+      <div class="flex gap-8 justify-center py-6">
+        <div v-for="(data, index) in dataDescriptions" :key="index">
+          <CardsForParentDescription
+            :title="data.title"
+            :description="data.description"
+            :icon="data.icon"
+            :iconSize="data.iconSize"
+          />
         </div>
       </div>
     </div>
     <div id="bg" class="absolute top-0 left-0 h-full w-full bg-black">
       <img
         src="/assets/for-parent_bg.png"
-        class="w-full h-full object-cover opacity-60"
+        class="w-full h-full object-cover opacity-40"
       />
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const dataDescriptions = [
+  {
+    title: "Detailed Progress Reports",
+    description:
+      "Gain insights into your child's academic performance, including completed modules, test scores, and time spent on learning",
+    icon: "solar:chart-bold-duotone",
+    iconSize: "1.6em",
+  },
+  {
+    title: "Personalized Feedback",
+    description:
+      "Identify areas where your child excels and where they may need extra support, with targeted recommendations for improvement",
+    icon: "solar:pen-new-square-bold-duotone",
+    iconSize: "1.6em",
+  },
+];
+</script>
