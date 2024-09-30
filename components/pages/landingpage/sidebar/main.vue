5a6ab3c40b73c3deecd9bdc6bb155a0365bcce18
rafiarrafif <rafiarrafif05@gmail.com>
2024-09-30 15:04:23 +0700

    add route to login in sidebar mobile

--- components/pages/landingpage/sidebar/main.vue
+++ components/pages/landingpage/sidebar/main.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="flex flex-col gap-1 mt-8">
-    <button
-      @click="openCartPage"
-      class="flex gap-3 px-3 py-2 rounded-lg hover:bg-darkGray-200"
-    >
-      <Icon name="solar:cart-large-2-linear" size="1.5em" />
-      <span class="font-medium">Cart</span>
-    </button>
-    <button
-      @click="changeLayout('langComponent')"
-      class="flex gap-3 px-3 py-2 rounded-lg hover:bg-darkGray-200"
-    >
-      <Icon name="solar:earth-linear" size="1.4em" />
-      <span class="font-medium">Language</span>
-    </button>
+  <div class="flex flex-col justify-between mt-8 mb-16 h-full">
+    <div class="flex flex-col gap-1">
+      <button
+        @click="openCartPage"
+        class="flex gap-3 px-3 py-2 rounded-lg hover:bg-darkGray-200"
+      >
+        <Icon name="solar:cart-large-2-linear" size="1.5em" />
+        <span class="font-medium">Cart</span>
+      </button>
+      <button
+        @click="changeLayout('langComponent')"
+        class="flex gap-3 px-3 py-2 rounded-lg hover:bg-darkGray-200"
+      >
+        <Icon name="solar:earth-linear" size="1.4em" />
+        <span class="font-medium">Language</span>
+      </button>
+    </div>
+    <div class="flex flex-col">
+      <button
+        class="py-3 text-sm font-medium w-full bg-primary-950 text-primary-50 rounded-lg"
+      >
+        {{ $t("signUp") }}
+      </button>
+      <button
+        @click="toLogin"
+        class="py-3 text-sm font-medium w-full bg-transparent text-primary-950 rounded-lg"
+      >
+        {{ $t("logIn") }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -26,5 +41,13 @@ const changeLayout = (component: string) => {
 };
 const openCartPage = () => {
   landingPageStore.toggleCartPageState();
+};
+
+import { useLoginCompStore } from "#imports";
+const loginCompStore = useLoginCompStore();
+const toLogin = () => {
+  landingPageStore.toggleSidebarState();
+  window.history.pushState(null, "", "/login");
+  loginCompStore.toggleLoginVisibility(true);
 };
 </script>
